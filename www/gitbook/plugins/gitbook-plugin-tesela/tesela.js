9515a148552f49de24a173502fa54286ffae79f9
Eduardo Abeliuk <eabeliuk@gmail.com>
2014-07-25 21:12:41 -0700

    another fix, issue #2

--- www/gitbook/plugins/gitbook-plugin-tesela/tesela.js
+++ www/gitbook/plugins/gitbook-plugin-tesela/tesela.js
@@ -1,13 +1,12 @@
-$( document ).ready(function() {
-        $(".summary > .chapter > a").click(function() {
-                $(this).parent().find('> ul').slideToggle();
-                return false;
-            });
-
-        $(".chapter ").find('> a').first().on('click', function(){
-		window.location = "/manual/";   
-	    });
-
-
-        $(".summary > .chapter > a").parent().find('> ul').hide();
-    });
+//$( document ).ready(function() {
+//        $(".summary > .chapter > a").click(function() {
+//                $(this).parent().find('> ul').slideToggle();
+//                return false;
+//           });
+//
+//      $(".chapter ").find('> a').first().on('click', function(){
+//	window.location = "/manual/";   
+//    });
+//
+//        $(".summary > .chapter > a").parent().find('> ul').hide();
+//  });
